09212ae90814dd6113a8b853aa28143b794964bb
i2sane <i2sane@proton.me>
2026-07-22 13:55:14 -0500

    aaaa

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <raylib.h>
+#include "fifo.h"
 
-struct gameState {
+typedef struct {
 	bool isInElevator;
 	Rectangle *levelWalls[10000];
-}
+} GameState;
 
 int main(int argc, char **argv) {
+	puts("UTTG!");
 	
 	return 0;
 }
